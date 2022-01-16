a2802c7553dde7e5ff635d8f581fed0098bc703f
zyt734362064 <734362064@qq.com>
2022-01-16 22:06:31 +0800

    practice commit git

---
+++ src/com/Test0116/updata.java
@@ -0,0 +1,13 @@
+package com.Test0116;
+
+/**
+ * Create with IntelliJ IDEA
+ * Description:
+ * User:Zyt
+ * Date:2022-01-16
+ */
+public class updata {
+    public static void main(String[] args) {
+
+    }
+}
