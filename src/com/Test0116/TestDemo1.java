c9bb9bcbafe50f61fafc97ba1b3e8f4f213d42d2
zyt734362064 <734362064@qq.com>
2022-01-16 21:48:06 +0800

    practice commit git

---
+++ src/com/Test0116/TestDemo1.java
@@ -0,0 +1,28 @@
+package com.Test0116;
+
+import java.util.Arrays;
+
+/**
+ * Create with IntelliJ IDEA
+ * Description:
+ * User:Zyt
+ * Date:2022-01-16
+ */
+public class TestDemo1 {
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,4,5,6};
+        int[] arr2 = {2,4,5,6,8,9};
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+        func1(arr1,arr2);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+    }
+    public static void func1(int[] arr1,int[] arr2){
+        for (int i = 0;i < arr1.length;i++){
+            int tmp = arr1[i];
+            arr1[i] = arr2[i];
+            arr2[i] = tmp;
+        }
+    }
+}
