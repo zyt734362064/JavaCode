ba269a9880232e0eadda2b790f052912ab02d469
zyt734362064 <734362064@qq.com>
2022-03-14 20:34:38 +0800

    多态和抽象类

--- src/com/Test0314/Demo3/Test2.java
+++ src/com/Test0314/Demo3/Test2.java
@@ -1,13 +1,34 @@
 package com.Test0314.Demo3;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 /**
  * Create with IntelliJ IDEA
- * Description:
+ * Description:接口
  * User:Zyt
  * Date:2022-03-14
  */
-interface A {
+interface IA {
     public final int A = 10;
+
+    public void funcA();
+}
+
+interface IB{
+    public void funcB();
+}
+
+class AClass implements IA,IB{
+    @Override
+    public void funcA() {
+        System.out.println("A::funcA()");
+        System.out.println(A);
+    }
+
+    @Override
+    public void funcB() {
+        System.out.println("A::funcB");
+    }
 }
 
 public class Test2 {
---
+++ src/com/Test0314/Demo3/Test3.java
@@ -0,0 +1,17 @@
+package com.Test0314.Demo3;
+
+/**
+ * Create with IntelliJ IDEA
+ * Description:
+ * User:Zyt
+ * Date:2022-03-14
+ */
+interface IA1{
+    void funcA();
+}
+
+interface IB1 extends IA1{
+    void funcB();
+}
+public class Test3 {
+}
